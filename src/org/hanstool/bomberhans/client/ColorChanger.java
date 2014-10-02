2f6d35d204886e3b411f3169d60972405da3c1bd
Wonko <wonko@hanstool.org>
2014-10-02 22:14:00 +0200

    fix Color Changer

--- src/org/hanstool/bomberhans/client/ColorChanger.java
+++ src/org/hanstool/bomberhans/client/ColorChanger.java
@@ -12,48 +12,57 @@ import java.awt.image.RGBImageFilter;
 
 public class ColorChanger
 {
+
 	public static final int	ALPHA		= 0;
 	public static final int	RED			= 1;
 	public static final int	GREEN		= 2;
 	public static final int	BLUE		= 3;
+
 	public static final int	HUE			= 0;
 	public static final int	SATURATION	= 1;
 	public static final int	BRIGHTNESS	= 2;
+
 	public static final int	TRANSPARENT	= 0;
 
 	private static BufferedImage imageToBufferedImage(Image image)
 	{
-		BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), 2);
+
+		BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2 = bufferedImage.createGraphics();
 		g2.drawImage(image, 0, 0, null);
 		g2.dispose();
 
 		return bufferedImage;
+
 	}
 
-	public static BufferedImage makeColorTransparent(BufferedImage im, Color color)
+	public static BufferedImage makeColorTransparent(BufferedImage im, final Color color)
 	{
 		ImageFilter filter = new RGBImageFilter() {
-			public int	markerRGB;
+
+			// the color we are looking for... Alpha bits are set to opaque
+			public int	markerRGB	= color.getRGB() | 0xFF000000;
 
 			@Override
 			public final int filterRGB(int x, int y, int rgb)
 			{
 				if((rgb | 0xFF000000) == markerRGB)
 				{
-					return 0xFFFFFF & rgb;
+					// Mark the alpha bits as zero - transparent
+					return 0x00FFFFFF & rgb;
 				}
-
+				// nothing to do
 				return rgb;
 			}
 		};
+
 		ImageProducer ip = new FilteredImageSource(im.getSource(), filter);
 		return imageToBufferedImage(Toolkit.getDefaultToolkit().createImage(ip));
 	}
 
 	public BufferedImage changeColor(BufferedImage image, Color mask, Color replacement)
 	{
-		BufferedImage destImage = new BufferedImage(image.getWidth(), image.getHeight(), 2);
+		BufferedImage destImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
 
 		Graphics2D g = destImage.createGraphics();
 		g.drawImage(image, null, 0, 0);
@@ -63,6 +72,7 @@ public class ColorChanger
 		{
 			for(int j = 0; j < destImage.getHeight(); j++ )
 			{
+
 				int destRGB = destImage.getRGB(i, j);
 
 				if(matches(mask.getRGB(), destRGB))
@@ -79,7 +89,7 @@ public class ColorChanger
 	private float[] getHSBArray(int rgb)
 	{
 		int[] rgbArr = getRGBArray(rgb);
-		return Color.RGBtoHSB(rgbArr[1], rgbArr[2], rgbArr[3], null);
+		return Color.RGBtoHSB(rgbArr[RED], rgbArr[GREEN], rgbArr[BLUE], null);
 	}
 
 	private int getNewPixelRGB(int replacement, int destRGB)
@@ -87,13 +97,13 @@ public class ColorChanger
 		float[] destHSB = getHSBArray(destRGB);
 		float[] replHSB = getHSBArray(replacement);
 
-		int rgbnew = Color.HSBtoRGB(replHSB[0], replHSB[1], destHSB[2]);
+		int rgbnew = Color.HSBtoRGB(replHSB[HUE], replHSB[SATURATION], destHSB[BRIGHTNESS]);
 		return rgbnew;
 	}
 
 	private int[] getRGBArray(int rgb)
 	{
-		return new int[] { rgb >> 24 & 0xFF, rgb >> 16 & 0xFF, rgb >> 8 & 0xFF, rgb & 0xFF };
+		return new int[] { rgb >> 24 & 0xff, rgb >> 16 & 0xff, rgb >> 8 & 0xff, rgb & 0xff };
 	}
 
 	private boolean matches(int maskRGB, int destRGB)
@@ -101,10 +111,12 @@ public class ColorChanger
 		float[] hsbMask = getHSBArray(maskRGB);
 		float[] hsbDest = getHSBArray(destRGB);
 
-		if(hsbMask[0] == hsbDest[0])
+		if(hsbMask[HUE] == hsbDest[HUE])
 		{
+
 			return true;
 		}
 		return false;
 	}
+
 }
